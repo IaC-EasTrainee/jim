53640a83d2992135d639ebb8ac35bffc86b9ba2d
williamt1997 <william@enterpriseautomation.co.uk>
2025-05-14 16:13:58 +0100

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+
+# provider "kubernetes" {}
+#
+# module "runner_secrets" {
+#     source = "git::https://github.com/Enterprise-Automation/module-vault-integration"
+#
+#     secret_variables = [
+#         {
+#             name =
+#             vault_path =
+#             template_content =
+#         },
+#
+#         # Add More Secrets Here
+#         # I.e
+#         # {name = "", vault_path = "", template_content = ""}
+#     ]
+# }
+
